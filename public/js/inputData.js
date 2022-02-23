c1c2ff1d237e9451d20ce53a092fb18a19998a67
Davi Fabiano <45244185+SaraivaMelo@users.noreply.github.com>
2022-02-22 23:30:33 -0300

    frontend - 0.0.6 - criado os boxes e aplicado o JS - falta concluir

--- public/js/inputData.js
+++ public/js/inputData.js
@@ -1,4 +1,33 @@
 
-function sendNC(nc){
-    console.log(nc)
+function sendNC(){
+   let valueSelect =  document.querySelector('#valueSelect').value
+
+   if(valueSelect === "nc"){
+       document.querySelector('.ncValue').style.display = 'flex'
+    }else{
+       document.querySelector('.ncValue').style.display = 'none'
+   }
+
+}
+
+
+// if(sizeInput.search('/[@]/g')) {
+//     paper.push(sizeInput.value)
+// }
+
+function checkSize(){
+    let paper = []
+    let sizeInput = document.querySelector("#inputNCValue").value
+
+    if(sizeInput.length > 10){
+        document.querySelector(".addNC").style.background = "#008000"
+        document.querySelector('.ncValue').style.display = 'none'
+        paper.push(sizeInput)
+        insertNC(paper)
+    }
+}
+
+
+function insertNC(paper){
+    console.log(paper)
 }
